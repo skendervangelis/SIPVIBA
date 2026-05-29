41181c10bde5ff76a7d8871c3b908cee9f3a29ea
Skender Vangelis <skender.vangelis@gmail.com>
2026-05-28 18:11:55 -0600

    chore: Restaurar rutas para despliegue nativo en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://skendervangelis.github.io',
-  base: '/SIPVIBA',
   vite: {
     plugins: [tailwindcss()]
   }
